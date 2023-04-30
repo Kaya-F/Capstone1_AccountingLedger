94e1b966e75ce259775fc598c25fa2daf44a54a8
Kaya-F <kfisher@appdev.yearup.org>
2023-04-30 02:46:10 -0400

    menu structure - bare bones

--- src/main/java/com/kf/Ledger.java
+++ src/main/java/com/kf/Ledger.java
@@ -11,15 +11,14 @@ public class Ledger {
         // Create do-while loop that runs unless the user enters "4"
         do {// Create main menu:
             System.out.println("| Home |");
-            System.out.println("\tD.Add a Deposit ");
-            System.out.println("\tP.Make Payment (Debit)");
-            System.out.println("\tL. Ledger");
-            System.out.println("\tX. Exit.");
+            System.out.println("\tD) Add a Deposit ");
+            System.out.println("\tP) Make Payment (Debit)");
+            System.out.println("\tL) Ledger");
+            System.out.println("\tX) Exit.");
             System.out.print("");
 
             userInput = scanner.nextLine();
 
-            // Show prompt for user showing the possible options and asking for an input
             // Create switch statement that handles cases related to each menu item
             switch (userInput){
                 case "D":
@@ -44,5 +43,44 @@ public class Ledger {
 
     public static void makePayment(){}
 
-    public static void openLedger(){}
+    public static void openLedger(){
+        String userInput;
+        do {
+            System.out.println("| Ledger |");
+            System.out.println("\tA) All"); // displays all entries
+            System.out.println("\tD) Deposits"); // only displays the entries that are deposits into the account
+            System.out.println("\tP) Payments");// only displays the negative entries/payments
+            System.out.println("\tR) Reports"); // screen allows user to run pre-defined reports or to run a custom search
+            System.out.print("Command: ");
+
+            userInput = scanner.nextLine();
+
+            switch(userInput){ // Ledger Menu
+                case "A":
+                    displayAll();
+                    break;
+                case "D":
+                    displayDeps();
+                    break;
+                case "P":
+                    displayPays();
+                    break;
+                case "R":
+                    runReports();
+                    break;
+                default:
+                    System.out.println("Input command not found.");
+            }
+
+        } while (!userInput.equalsIgnoreCase("3"));
+
+    }
+    public static void displayAll(){
+
+    }
+    public static void displayDeps(){}
+    public static void displayPays(){}
+    public static void runReports(){}
+
+
 }
